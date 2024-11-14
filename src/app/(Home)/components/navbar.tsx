2a59c8b41fa4145c3f2377198a12ec07e099b798
andhikarsurya <andhikarsurya@gmail.com>
2024-11-14 23:37:08 +0700

    hi

--- src/app/(Home)/components/navbar.tsx
+++ src/app/(Home)/components/navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar(){
     <nav>
         <ul className="w-screen flex gap-[1rem] justify-between items-center p-[1rem] bg-green-800 text-white shadow-xl opacity-90">
             <div>
-            <img src="logo_icon.png" alt="" className="w-[3rem] absolute top-[0.3rem] left-[1rem] cursor-pointer"/></div>
+            <img src="logo_icon.webp" alt="" className="w-[3rem] absolute top-[0.3rem] left-[1rem] cursor-pointer"/></div>
             <div className="flex gap-[1rem] justify-center items-center ml-[3rem]">
             <li className="hover:scale-[140%] duration-200 hover:px-[1rem]"><Link href={"/"}>Home</Link></li>
             <li className="hover:scale-[140%] duration-200 hover:px-[1rem]"><Link href={"/product"}>Product</Link></li>
